51f67c6d1cbc6d09831a386e5b6bc9bd3a1ca3b6
Nikoloz Muladze <nikoloz>
2023-01-31 03:40:45 +0400

    chalk

---
+++ Chalk Module/index.js
@@ -0,0 +1,8 @@
+// Importing module
+// If you are using TypeScript, use "npm i chalk@4.1.2"
+const chalk = require("chalk");
+
+// Printing the text
+console.log(chalk.red("aayush"));
+console.log(chalk.red.underline("aayush"));
+console.log(chalk.red.underline.bold("GFG"));
