5d282028d7d9fc15be8ea0d11af5d7d73158c2a2
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-03-24 04:46:55 +0400

    switched to .pem files

--- Authentications/JWT TS/src/utils/jwt.ts
+++ Authentications/JWT TS/src/utils/jwt.ts
@@ -1,11 +1,13 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
 import config from 'config';
+import fs from 'fs';
+
+const privateKeyPath: string = process.cwd() + '/config/private_key.pem';
+const publicKeyPath: string = process.cwd() + '/config/public_key.pem';
 
 export const signJwt = (payload: Object, options: SignOptions = {}) => {
-  const privateKey = Buffer.from(
-    config.get<string>('accessTokenPrivateKey'),
-    'base64'
-  ).toString('ascii');
+  console.log();
+  const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
   return jwt.sign(payload, privateKey, {
     ...(options && options),
     algorithm: 'RS256',
@@ -14,10 +16,7 @@ export const signJwt = (payload: Object, options: SignOptions = {}) => {
 
 export const verifyJwt = <T>(token: string): T | null => {
   try {
-    const publicKey = Buffer.from(
-      config.get<string>('accessTokenPublicKey'),
-      'base64'
-    ).toString('ascii');
+    const publicKey = fs.readFileSync(publicKeyPath, 'utf8');
     return jwt.verify(token, publicKey) as T;
   } catch (error: any) {
     console.error(error.message);
